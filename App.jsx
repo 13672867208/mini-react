4de082a7112b6cc9c7ddd3f2bc9539aebab3a3ea
bin <13782867208>
2024-01-16 22:38:29 +0800

    添加适配 function component

--- App.jsx
+++ App.jsx
@@ -1,4 +1,18 @@
 import React from "./core/react";
-const App = <div>13123</div>
+
+function Counter({num}){
+    return  <div>{num}</div>
+}
+function SecondCount({num}){
+    return <Counter num={num}></Counter>
+}
+
+const App = <div>
+ 123123
+ <div>123</div>
+ <Counter num={34}></Counter>
+ <SecondCount num={20}></SecondCount>
+ 
+    </div> 
 console.log(App)
 export default App
--- core/react.js
+++ core/react.js
@@ -23,7 +23,7 @@ function createElement(type,props,...children){
             ...props,
             children:children.map(e=>{
                 console.log(e,'66555',typeof e)
-               return typeof e==='string'?createElementText(e):e
+               return typeof e==='string'||typeof e==='number'?createElementText(e):e
             })
         }
         
@@ -55,6 +55,59 @@ function createProps(dom,props){
         }
     })
 }
+// 生成链表
+function initChildren(children,fiber){
+    let prevChild =null // 记录上一个儿子
+    children?.forEach((child,index)=>{
+        const obj = {
+            type:child.type,
+            props:child.props,
+            parent:fiber,
+            subling:null,
+            child:null,
+            dom:null
+        }
+       if(index===0){
+        fiber.child = obj
+       }else{
+        prevChild.subling = obj
+       }
+       prevChild = obj
+    })
+}
+
+// 更新function组件
+function updateFunctionCompoent(fiber){
+   console.log(fiber.type(fiber.props),'asdasdasd') 
+   const children = fiber.type(fiber.props)
+   initChildren([children],fiber)
+   console.log(fiber,'udadasdasd')
+
+}
+
+
+// 更新非function 组件
+
+function updateHostComponent(fiber){
+    if(!fiber.dom){
+        //  1.创建dom  
+        const dom =fiber.dom =  createDom(fiber.type)
+  
+        // 2.创建dom 的属性  
+        createProps(dom,fiber.props)   
+      }
+  
+      // 3.变成链表   
+          // parent
+          // subling
+          // child
+          const children = fiber.props.children
+          initChildren(children,fiber)
+}
+
+
+
+
 // fiber 是个链表对象
 /**
  * 
@@ -62,43 +115,22 @@ function createProps(dom,props){
  * @returns 
  */
 function performWorkofUnit(fiber){
-    if(!fiber.dom){
-      //  1.创建dom  
-      const dom =fiber.dom =  createDom(fiber.type)
-
-      // 2.创建dom 的属性  
-      createProps(dom,fiber.props)   
-    }
-
-    // 3.变成链表   
-        // parent
-        // subling
-        // child
-        const children = fiber.props.children
-        let prevChild =null // 记录上一个儿子
-        children?.forEach((child,index)=>{
-            const obj = {
-                type:child.type,
-                props:child.props,
-                parent:fiber,
-                subling:null,
-                child:null,
-                dom:null
-            }
-           if(index===0){
-            fiber.child = obj
-           }else{
-            prevChild.subling = obj
-           }
-           prevChild = obj
-        })
+    console.log(fiber,fiber.type,'123123')
+    // fiber.
+    typeof fiber.type==='function'?updateFunctionCompoent(fiber):updateHostComponent(fiber)
+      
     // 4.返回内容
         // 有儿子先返回儿子
         if(fiber.child) return fiber.child
         // 没有儿子的情况下返回兄弟层级的
-        if(fiber.subling) return fiber.subling
-        // 没有儿子也没有兄弟的情况下返回父子的兄弟
-        return fiber.parent?.subling
+        // if(fiber.subling) return fiber.subling
+        // // 没有儿子也没有兄弟的情况下返回父子的兄弟 递归防止是function component 的时候又一层是空
+        let nextFiber = fiber
+        while(nextFiber){
+            if(nextFiber.subling){ return nextFiber.subling}
+            nextFiber = nextFiber.parent
+        }
+    
 }
 
 
@@ -124,17 +156,24 @@ function commitRoot(){
     console.log(root,'455')
     // root = null
 }
+
 // 添加到子节点
 function commitWork(fiber){
-    fiber.parent.dom.append(fiber.dom)
+        // fiber.parent.dom.append(fiber.dom)
+    let parentFiber = fiber.parent
+    while(!parentFiber.dom){
+        parentFiber = parentFiber.parent
+    }
+    if(fiber.dom){
+        parentFiber.dom.append(fiber.dom)
+    }
+    // console.log(fiber.child,'---')
+    // 疯狂递归儿子
     if(fiber.child){
         commitWork(fiber.child)
     }
+     // 疯狂递归兄弟
     if(fiber.subling){
-        commitWork(fiber.subling)
-    }
-    let parentFiber = fiber.parent
-    while(fiber.parent.subling){
         commitWork(fiber.subling)
     }
 }
